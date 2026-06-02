b9d36e6e3988d438208fd36d090469dbc654e3c1
Swayam <swayambhajan1@gmail@example.com>
2026-06-02 08:24:47 +0530

    update API URL for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,8 @@ origins = [
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
-    "https://inventory-system-e24t.vercel.app/products",
+    "https://inventory-system-iota-sage.vercel.app",
+    
     # 👉 later replace with your final Vercel domain
 ]
 
